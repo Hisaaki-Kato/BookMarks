7663a04beb95248a8c8911185384d3fd4ada89e8
hisaakikato <hisaakikato.work@gmail.com>
2020-06-15 21:36:43 +0900

    Finished setup for rspec.

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,13 @@ module BookMarks
     
     # 認証トークンをremoteフォームに埋め込む
     config.action_view.embed_authenticity_token_in_remote_forms = true
+
+    config.generators do |g|
+      g.test_framework :rspec,
+        fixtures: false,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false
+      end
   end
 end
