02e4d9683b7bdbd8908eddb562284c3190beb737
hisaakikato <hisaakikato.work@gmail.com>
2020-06-17 12:50:55 +0900

    Finished test for likes_controller.

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,5 +1,7 @@
 class LikesController < ApplicationController
-  before_action :logged_in_user
+  before_action :logged_in_user, only: [:create, :destroy]
+  before_action :not_owner,      only: :create
+  before_action :correct_user,   only: :destroy
 
   def create
     @micropost = Micropost.find(params[:micropost_id])
@@ -20,4 +22,16 @@ class LikesController < ApplicationController
       format.js
     end
   end
+
+  private
+
+    def not_owner
+      micropost = Micropost.find(params[:micropost_id])
+      redirect_to root_url if micropost.user == current_user
+    end
+
+    def correct_user
+      like = Like.where(micropost_id: params[:micropost_id], user_id: current_user.id)
+      redirect_to root_url if like.blank?
+    end
 end
