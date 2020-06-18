07e6ff5f206acb0f531c88fd35aa0b3cb184c38f
hisaakikato <hisaakikato.work@gmail.com>
2020-06-18 13:37:16 +0900

    Finished feature-specs for micropost.

--- Gemfile
+++ Gemfile
@@ -72,7 +72,8 @@ group :test do
   gem 'guard-minitest',           '2.4.4'
   # Adds support for Capybara system testing and selenium driver
   gem 'capybara', '>= 2.15'
-  gem 'selenium-webdriver'
+  gem 'launchy'
+  gem 'webdrivers'
   # Easy installation and use of chromedriver to run system tests with Chrome
   gem 'chromedriver-helper'
 end
---
+++ spec/features/microposts_spec.rb
@@ -0,0 +1,61 @@
+require 'rails_helper'
+
+RSpec.feature "Microposts", type: :feature do
+  #許可されたユーザーとして
+  context "as an authorized user" do
+    before do
+      @user = create(:user)
+      @book = create(:book)
+      log_in_as(@user)
+    end
+    #ユーザーは新しいポストを作成する
+    scenario "user creates a new micropost" do
+      expect {
+        visit book_path(@book.id)
+        fill_in "micropost[quoted_text]",	with: "test post"
+        fill_in "micropost[content]",	with: "test content"
+        click_button "メモを投稿する"
+
+        expect(page).to have_content "test post"
+        expect(page).to have_content "test content"
+      }.to change(@user.microposts, :count).by(1)
+    end
+
+    #正しくない内容ではポストは作成されない
+    scenario "user does not create a post with invalid content" do
+      expect {
+        visit book_path(@book.id)
+        fill_in "micropost[quoted_text]",	with: nil
+        fill_in "micropost[content]",	with: "test content"
+        click_button "メモを投稿する"
+
+        expect(page).to have_content "正しい内容を入力してください"
+        expect(page).to_not have_content "test content"
+      }.to_not change(@user.microposts, :count)
+    end
+
+    #ユーザーは自分のポストを削除する
+    scenario "user deletes own micropost" do
+      micropost = create(:micropost, user_id: @user.id, book_id: @book.id)
+      expect {
+        visit book_path(@book.id)
+        click_link "メモを削除する"
+        expect(page).to have_content "学びメモを削除しました。"
+      }.to change(@user.microposts, :count).by(-1)
+    end
+  end
+
+  #ゲストユーザーとして
+  context "as a guest user" do
+    before do
+      @book = create(:book)
+    end
+
+    #ポストの投稿フォームが表示されない
+    scenario "user can not find the micropost form" do
+      visit book_path(@book.id)
+      expect(page).to_not have_content "メモを投稿する"
+      expect(page).to have_content "みんなの学びメモ"
+    end
+  end
+end
