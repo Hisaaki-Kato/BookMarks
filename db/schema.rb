9cb4375c968218a4fee2c63452170e3a0184ebde
hisaakikato <hisaakikato.work@gmail.com>
2020-06-22 01:00:55 +0900

    Fixed gemfiles

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,84 +10,86 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_200_609_053_529) do
-  create_table 'boards', force: :cascade do |t|
-    t.text 'content'
-    t.integer 'user_id'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.integer 'book_id'
-    t.string 'title'
-    t.index %w[user_id created_at], name: 'index_boards_on_user_id_and_created_at'
-    t.index ['user_id'], name: 'index_boards_on_user_id'
+ActiveRecord::Schema.define(version: 2020_06_09_053529) do
+
+  create_table "boards", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "book_id"
+    t.string "title"
+    t.index ["user_id", "created_at"], name: "index_boards_on_user_id_and_created_at"
+    t.index ["user_id"], name: "index_boards_on_user_id"
   end
 
-  create_table 'books', force: :cascade do |t|
-    t.string 'title'
-    t.string 'image'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.string "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'comments', force: :cascade do |t|
-    t.integer 'user_id'
-    t.integer 'micropost_id'
-    t.text 'content'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['created_at'], name: 'index_comments_on_created_at'
-    t.index %w[user_id micropost_id created_at], name: 'index_comments_on_user_id_and_micropost_id_and_created_at'
+  create_table "comments", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "micropost_id"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["created_at"], name: "index_comments_on_created_at"
+    t.index ["user_id", "micropost_id", "created_at"], name: "index_comments_on_user_id_and_micropost_id_and_created_at"
   end
 
-  create_table 'likes', force: :cascade do |t|
-    t.integer 'user_id'
-    t.integer 'micropost_id'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['micropost_id'], name: 'index_likes_on_micropost_id'
-    t.index %w[user_id micropost_id], name: 'index_likes_on_user_id_and_micropost_id', unique: true
-    t.index ['user_id'], name: 'index_likes_on_user_id'
+  create_table "likes", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "micropost_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["micropost_id"], name: "index_likes_on_micropost_id"
+    t.index ["user_id", "micropost_id"], name: "index_likes_on_user_id_and_micropost_id", unique: true
+    t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
-  create_table 'microposts', force: :cascade do |t|
-    t.text 'content'
-    t.integer 'user_id'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.integer 'book_id'
-    t.text 'quoted_text'
-    t.index %w[user_id created_at], name: 'index_microposts_on_user_id_and_created_at'
-    t.index ['user_id'], name: 'index_microposts_on_user_id'
+  create_table "microposts", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "book_id"
+    t.text "quoted_text"
+    t.index ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
+    t.index ["user_id"], name: "index_microposts_on_user_id"
   end
 
-  create_table 'reads', force: :cascade do |t|
-    t.integer 'user_id'
-    t.integer 'book_id'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['book_id'], name: 'index_reads_on_book_id'
-    t.index ['user_id'], name: 'index_reads_on_user_id'
+  create_table "reads", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "book_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["book_id"], name: "index_reads_on_book_id"
+    t.index ["user_id"], name: "index_reads_on_user_id"
   end
 
-  create_table 'relationships', force: :cascade do |t|
-    t.integer 'follower_id'
-    t.integer 'followed_id'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['followed_id'], name: 'index_relationships_on_followed_id'
-    t.index %w[follower_id followed_id], name: 'index_relationships_on_follower_id_and_followed_id', unique: true
-    t.index ['follower_id'], name: 'index_relationships_on_follower_id'
+  create_table "relationships", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["followed_id"], name: "index_relationships_on_followed_id"
+    t.index ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
+    t.index ["follower_id"], name: "index_relationships_on_follower_id"
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'name'
-    t.string 'email'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.string 'password_digest'
-    t.boolean 'admin', default: false
-    t.text 'profile'
-    t.string 'picture'
-    t.index ['email'], name: 'index_users_on_email', unique: true
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "password_digest"
+    t.boolean "admin", default: false
+    t.text "profile"
+    t.string "picture"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
+
 end
