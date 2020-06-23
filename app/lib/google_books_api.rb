b36a30681a41ea953fae13080a7eef9c36872768
hisaakikato <hisaakikato.work@gmail.com>
2020-06-23 11:30:08 +0900

    Fixed book search function for images

--- app/lib/google_books_api.rb
+++ app/lib/google_books_api.rb
@@ -14,7 +14,7 @@ module GoogleBooksApi
       count.times do |x|
         results[x][0] = response.dig('items', x, 'volumeInfo', 'title')
         results[x][1] = response.dig('items', x, 'volumeInfo', 'imageLinks', 'thumbnail')
-        results[x][1] = results[x][1].gsub!('http', 'https')
+        results[x][1] = results[x][1].gsub!('http', 'https') if !results[x][1].nil?
         results[x][2] = response.dig('items', x, 'volumeInfo', 'authors')
         results[x][2] = results[x][2].join(',') if results[x][2] # 複数著者をカンマで区切る
       end
