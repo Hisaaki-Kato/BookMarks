6dd0ea019f0884bd719917bcf026813e40b10039
hisaakikato <hisaakikato.work@gmail.com>
2020-06-30 15:08:27 +0900

    Fixed seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,3 @@
-# frozen_string_literal: true
-
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 User.create!(name: 'Admin User',
              email: 'Admin@example.com',
              password: 'foobar',
@@ -33,35 +24,3 @@ users = User.order(:created_at).take(2)
                                               content: content_post,
                                               book_id: 1) }
 end
-
-# #board
-# title = 'board-title'
-# content_board = 'testboard'
-# users.each { |user|
-#   user.boards.create!(title: title,content: content_board,
-#                                    book_id: 1)
-#   user.boards.create!(title: title,content: content_board + '2',
-#                                    book_id: 2)}
-
-# #comment
-# users = User.order(:created_at).take(3)
-# posts = Micropost.order(:created_at).take(10)
-# 1.times do
-#   content_comment = 'testcomment'
-#   users.each {
-#     |user| user.comments.create!(micropost_id: 1,
-#                                  content: content_comment)
-#   }
-# end
-
-# #relationship
-# users = User.all
-# users[0].follow(users[1])
-# users[0].follow(users[2])
-# users[1].follow(users[0])
-
-# # user  = users.first
-# # following = users[1,2]
-# # followers = users[2]
-# # following.each { |followed| user.follow(followed) }
-# # followers.each { |follower| follower.follow(user) }
